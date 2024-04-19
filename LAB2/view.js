b90ad9d6353c7546d2c4cfb921c3c5874d677949
Zoriev Mykyta <chessking481@gmail.com>
2024-04-19 23:11:39 +0300

    File view.js changed.

--- LAB2/view.js
+++ LAB2/view.js
@@ -24,3 +24,18 @@ function calc()
     });
   };
 }
+
+function prof()
+{
+    var isLog = sessionStorage.getItem('logged');
+    var name = sessionStorage.getItem('name');
+    var email = sessionStorage.getItem('email');
+    
+    if(isLog == 1){
+        const id_name = document.getElementById("name");
+        id_name.textContent = name;
+        const id_email = document.getElementById("email");
+        id_email.textContent = email;
+        
+    }
+}
